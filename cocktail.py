e29e4a86bc1134907b65a8c31ad94e74bd7dd8f8
Louis Davies-Cren <44097632+louisdavcren@users.noreply.github.com>
2020-04-09 17:16:02 +0100

    proto2

---
+++ cocktail.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import webbrowser
+
+def userinput():
+    print("Please specify how you would like search for a drink:")
+    print("1 : Cocktail Name")
+    print("2 : Ingredient Name")
+    print("3 : Glass Name")
+
+    selected = input()
+
+    if(selected == "1"):
+        cocktailname()
+    if(selected == "2"):
+        ingredientname()
+    else: (print("Incorrect submission"))
+        
+
+def cocktailname():
+    print("Please enter the name of the cocktail: \n")
+    UserInput = input()
+    f = r"https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+UserInput
+    data = requests.get(f)
+    tt = json.loads(data.text)
+    #webbrowser.open(tt["url"])
+    
+    for i in (tt["drinks"]):
+        print(i["strDrink"], "\n")
+        print(i["strInstructions"], "\n")
+        
+        print(i["strIngredient1"])
+        print(i["strIngredient2"])
+        print(i["strIngredient3"])
+        print(i["strIngredient4"])
+
+
+def ingredientname():
+    print("Please enter ingredient: \n")
+    UserInput = input()
+    f = r" https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="+UserInput
+    data = requests.get(f)
+    tt = json.loads(data.text)
+    #webbrowser.open(tt["url"])
+
+    for i in (tt["drinks"]):
+        print(i["strDrink"], "\n")
+        print(i["strInstructions"], "\n")
+
+        print(i["strIngredient1"])
+        print(i["strIngredient2"])
+        print(i["strIngredient3"])
+        print(i["strIngredient4"])
+
+
+
+userinput()
