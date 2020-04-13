a9be636bf4737dac03ebde0a4ebe88ff9a2954be
c1834661 <davies-crenle@cardiff.ac.uk>
2020-04-13 15:05:27 +0100

    seperating

---
+++ dashboard.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import webbrowser
+from controlled_vocab import *
+
+print("Please specify how you would like search for a drink:")
+print("1 : Cocktail Name")
+print("2 : Ingredient Name")
+print("3 : Glass Name")
+
+selected = input()
+
+    # Function selector (there's probably a better way of doing this)
+if(selected == "1"):
+    cocktailname()
+if(selected == "2"):
+    ingredientname()
+else:
+    (print("Incorrect submission"))
