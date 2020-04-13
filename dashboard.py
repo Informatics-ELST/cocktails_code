c0d831394dd2e893b9099d4122fedd59dcaf27ca
c1834661 <davies-crenle@cardiff.ac.uk>
2020-04-13 15:57:19 +0100

    working dashboard

--- dashboard.py
+++ dashboard.py
@@ -5,25 +5,17 @@ from controlled_vocab import *
 from rapidproto import *
 
 
-menu = {}
-menu['1'] = "Add Student."
-menu['2'] = "Delete Student."
-menu['3'] = "Find Student"
-menu['4'] = "Exit"
-while True:
-    options = menu.keys()
-    options.sort()
-    for(menu.length):
-        print(entry, menu[entry])
+print("Please specify how you would like search for a drink:")
+print("1 : Cocktail Name")
+print("2 : Ingredient Name")
+print("3 : Glass Name")
 
-    selection = input("Please Select:")
-    if(selection == '1'):
-        print("add")
-    elif(selection == '2'):
-        print("delete")
-    elif(selection == '3'):
-        print("find")
-    elif(selection == '4'):
-        break
-    else:
-        print("Unknown Option Selected!")
+selected = input()
+
+    # Function selector (there's probably a better way of doing this)
+if(selected == "1"):
+    cocktailname()
+if(selected == "2"):
+    ingredientname()
+else:
+    (print("Incorrect submission"))
