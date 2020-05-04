78df8d031db33a16e7c305b2b48cc55107d9c13c
c1834661 <davies-crenle@cardiff.ac.uk>
2020-05-04 16:09:53 +0100

    get specific fixed (on call)

--- rapidproto.py
+++ rapidproto.py
@@ -76,40 +76,44 @@ def cocktail_name(user_input):
         for i in (tt["drinks"]):
             selector = selector+1
             #print("specific cocktails:", str(specific_cocktail))
-            print("______________________________________")
+            print("______________________________________\n")
 
             cocktail_name_var = str(i["strDrink"])
             specific_cocktail.append(str(i["strDrink"]))
 
-            print(str(selector) + ". " + "\nCocktail Name:   " +
-                  str(i["strDrink"]), "\n")
+            print(str(selector) + ". " +
+                  str(i["strDrink"]))
             # Printing the ingredients and their respective quantities.
-            print("Ingredients:")
 
             new_ingredient = ""
             ingredients_output_list = []
-            x = 1
-            while((str(i["strIngredient"+str(x)])) != "None"):
-                new_ingredient = str(i["strMeasure"+str(x)]) + \
-                    " of " + str(i["strIngredient"+str(x)])
-                ingredients_output_list.append(new_ingredient)
-                print(new_ingredient)
-                x += 1
-
-                # code for instructions (copied from surprise me
-            print("\nThis is how you make it: ")
-            instructions = str(i["strInstructions"])
-            formatted = instructions.split(". ")
-            j = 1
-            instructions_list = []
-            for x in formatted:
-                instructions_list.append(str(j)+". "+(x))
-                print(str(j)+". "+(x))
-                j += 1
+            
 
             print("______________________________________")
+        choice = input("Please choose the cocktail (by it's number): ")
+        cocktail_name_var = specific_cocktail[int(choice)]
 
-        get_specific_ingredients(specific_cocktail, selector)
+        print("\nCocktail Name: ", cocktail_name_var)
+        print("\nIngredients:")
+        x = 1
+        while((str(i["strIngredient"+str(x)])) != "None"):
+            new_ingredient = str(i["strMeasure"+str(x)]) + " of " + str(i["strIngredient"+str(x)])
+            ingredients_output_list.append(new_ingredient)
+            print(new_ingredient)
+            x += 1
+
+            # code for instructions (copied from surprise me
+        print("\nThis is how you make it: ")
+        instructions = str(i["strInstructions"])
+        formatted = instructions.split(". ")
+        j = 1
+        instructions_list = []
+        for x in formatted:
+            instructions_list.append(str(j)+". "+(x))
+            print(str(j)+". "+(x))
+            j += 1
+
+        #get_specific_ingredients(specific_cocktail, selector)
 
     machine_readable_outputs(cocktail_name_var, ingredients_output_list, instructions_list, instructions)
 
