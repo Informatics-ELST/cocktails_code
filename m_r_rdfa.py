d18eb70cf733edb11e7b683e75087b5f7bd7692e
c1834661 <davies-crenle@cardiff.ac.uk>
2020-05-08 18:01:53 +0100

    comments

--- m_r_rdfa.py
+++ m_r_rdfa.py
@@ -1,11 +1,6 @@
-#cocktail = name, author, date published, description, 
-
-
 def m_r_rdfa(cocktail_name, cocktail_ingredients_list, cocktail_instructions):
-    #cocktail_name = "espresso martini"
-    #cocktail_ingredients_list = ["3 vodka", "2 lemons", "1 coffee"]
-    #cocktail_instructions = "do this"
-
+    
+    # Creates a string of all of the ingredients to be able to print it all off
     ingredients_string = ""
     x = 0
     while x < len(cocktail_ingredients_list):
@@ -14,28 +9,7 @@ def m_r_rdfa(cocktail_name, cocktail_ingredients_list, cocktail_instructions):
         ingredients_string = ingredients_string+new_string
         x = x+1
 
-    #print("this is ingredients string: ", ingredients_string)
-    #print("<span property = 'name' > %s < /span > " % (drinkname,))
-    print(
-    """
-        <div vocab="http://schema.org/" typeof="Recipe">\n
-          <span property="name">"""+cocktail_name+"""</span>\n
-          </div>
-          Ingredients:
-        """+
-          str(ingredients_string),
-          #- <span property="recipeIngredient">3 or 4 ripe bananas, smashed</span>
-          #- <span property="recipeIngredient">1 egg</span>
-          #- <span property="recipeIngredient">3/4 cup of sugar</span>
-          
-    """Instructions:
-          <span property="recipeInstructions">
-          """+cocktail_instructions+"""
-          </span>
-        </div>
-    """
-    )
-
+    # Writes the entirety of the RDFa output into a HTML file - we have checked schema.org for formatting
     with open('cocktail_m_r_rdfa_output.html', 'w') as file:
         file.write('<div vocab="http://schema.org/" typeof="Recipe">\n')
         file.write('  <span property="name">'+cocktail_name+'</span>\n')
@@ -48,8 +22,7 @@ def m_r_rdfa(cocktail_name, cocktail_ingredients_list, cocktail_instructions):
         file.write('\n')
         file.write('   </span>\n')
         file.write('</div>')
-        #file.write('')
 
     file.close()
-
+    # Tells the user that a file has been made.
     print("\nWritten to HTML file as RDFa!\n\n")
